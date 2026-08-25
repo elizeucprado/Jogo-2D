ba841d420c3ad2faabb851805907750776566a67
elizeucprado <elizeucp01@gmail.com>
2026-08-25 12:14:34 -0300

    Create Scripts Folder & Test Script

---
+++ Assets/Scripts/Teste.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // ISSO É UM EXEMPLO DE CODIGO.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
